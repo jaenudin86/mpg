190a22f68ff8617bd0f675ff3e274706fccab6d6
andrew <radicalalbert@gmail.com>
2015-12-17 15:28:25 -0800

    added alertDialog before exporting; initTextView and initTextButton

--- app/src/main/java/com/a/b/mileagetracker/Fragments/AllHistoryFragment.java
+++ app/src/main/java/com/a/b/mileagetracker/Fragments/AllHistoryFragment.java
@@ -37,6 +37,7 @@ public class AllHistoryFragment extends Fragment implements View.OnClickListener
     private DialogInterfaces.DialogInterface mListener;
     public static HistoryCursorAdapter mHistoryCursorAdapter;
     private Button mEmptyButton;
+    private TextView mInitMessage;
 
     public AllHistoryFragment(){
     }
@@ -53,6 +54,7 @@ public class AllHistoryFragment extends Fragment implements View.OnClickListener
         mDBHelper=MySQLiteHelper.getInstance(getActivity().getApplicationContext());
         View view = inflater.inflate(R.layout.all_data_listview_fragment, container, false);
         mEmptyButton = (Button) view.findViewById(R.id.empty_history_button);
+        mInitMessage=(TextView) view.findViewById(R.id.init_message);
         mEmptyButton.setOnClickListener(this);
         mListView = (ListView) view.findViewById(R.id.listview);
 
@@ -62,13 +64,18 @@ public class AllHistoryFragment extends Fragment implements View.OnClickListener
         final Cursor cursor=mDBHelper.getAllData();
 
         if(cursor==null){
-            mEmptyButton.setText("Get Started");
+            mEmptyButton.setText(R.string.get_started);
             mEmptyButton.setVisibility(View.VISIBLE);
+            mInitMessage.setText(R.string.init_message_no_vehicles);
+            mInitMessage.setVisibility(View.VISIBLE);
         }else if (cursor.getCount()<1){
-            mEmptyButton.setText("Add Record");
+            mEmptyButton.setText(R.string.add_record_title);
             mEmptyButton.setVisibility(View.VISIBLE);
+            mInitMessage.setText(R.string.init_message_no_data);
+            mInitMessage.setVisibility(View.VISIBLE);
         }else{
             mEmptyButton.setVisibility(View.GONE);
+            mInitMessage.setVisibility(View.GONE);
         }
 
 //        mEmptyButton.setVisibility(cursor!=null?View.GONE:View.VISIBLE);
@@ -97,16 +104,20 @@ public class AllHistoryFragment extends Fragment implements View.OnClickListener
         Cursor cursor=mDBHelper.getAllData();
         mHistoryCursorAdapter.changeCursor(cursor);
         mHistoryCursorAdapter.notifyDataSetChanged();
-//        mEmptyButton.setVisibility(cursor != null ? View.GONE : View.VISIBLE);
-//        mEmptyButton.setText(cursor == null ? "Get Started" : "Add Record");
+
         if(cursor==null){
-            mEmptyButton.setText("Get Started");
+            mEmptyButton.setText(R.string.get_started);
             mEmptyButton.setVisibility(View.VISIBLE);
+            mInitMessage.setText(R.string.init_message_no_vehicles);
+            mInitMessage.setVisibility(View.VISIBLE);
         }else if (cursor.getCount()<1){
-            mEmptyButton.setText("Add Record");
+            mEmptyButton.setText(R.string.add_record_title);
             mEmptyButton.setVisibility(View.VISIBLE);
+            mInitMessage.setText(R.string.init_message_no_data);
+            mInitMessage.setVisibility(View.VISIBLE);
         }else{
             mEmptyButton.setVisibility(View.GONE);
+            mInitMessage.setVisibility(View.GONE);
         }
     }
 
--- app/src/main/java/com/a/b/mileagetracker/MainActivity.java
+++ app/src/main/java/com/a/b/mileagetracker/MainActivity.java
@@ -2,6 +2,7 @@ package com.a.b.mileagetracker;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.internal.view.menu.MenuBuilder;
 import android.text.TextUtils;
 import android.util.Log;
@@ -323,9 +325,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //            Intent emailIntent=new Intent(MainActivity.this,SendEmailActivity.class);
 //            emailIntent.putExtra("attachent","testStringAttachment");
 //            MainActivity.this.startActivity(emailIntent);
-            EmailFragment emailFragment = new EmailFragment();
-            ft.add(emailFragment, "emailFragment");
-            ft.commit();
+
+            new AlertDialog.Builder(this)
+                .setTitle("Export Data")
+                .setMessage("This will create a spreadsheet of all your collected data. You can send it by email to yourself in the next screens!")
+                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        FragmentTransaction ft = fragmentManager.beginTransaction();
+                        Log.e("Main", "clicked yep");
+                        EmailFragment emailFragment = new EmailFragment();
+                        ft.add(emailFragment, "emailFragment");
+                        ft.commit();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.e("main", "nope");
+                    }
+                })
+                .setIcon(R.mipmap.ic_launcher)
+                .show();
         }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
